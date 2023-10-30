55eb21ac185e3f6022df0ae64b808c04ae806e63
Vaibhav Tiwari <tvaibhav1204@gmail.com>
2023-10-30 19:34:03 +0530

    form submission and loader

--- scripts/main.js
+++ scripts/main.js
@@ -16,33 +16,40 @@
       easing: "ease-in-out",
       once: true,
       mirror: false,
-      disable: 'mobile'
+      
     });
   }
    // Add your javascript here
   (function ($) {
-    if ($('.typed-text-output').length == 1) {
-      var typed_strings = $('.typed-text').text();
-      var typed = new Typed('.typed-text-output', {
-          strings: typed_strings.split(', '),
-          typeSpeed: 50,
-          backSpeed: 20,
-          smartBackspace: false,
-          loop: true
-      });
-    }
-    $(".anchor-readMore").click(function(){
-      const anchorText = $(".anchor-readMore").text().trim();
-      console.log("Anchor Text", anchorText)
-      const setText = anchorText === 'Read more' ? 'Read less' : 'Read more';
-        setTimeout(function(){
-          $(".anchor-readMore").text(setText);
-        }, 200)
-      });
-      $('#scrolltop').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
-      });
+    $(window).on('load', function() {
+      $('#loading').slideUp(1000);
+  });
+    $(document).ready(function(){
+     
+    //Ajax Example
+      if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 50,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+      }
+      $(".anchor-readMore").click(function(){
+        const anchorText = $(".anchor-readMore").text().trim();
+        console.log("Anchor Text", anchorText)
+        const setText = anchorText === 'Read more' ? 'Read less' : 'Read more';
+          setTimeout(function(){
+            $(".anchor-readMore").text(setText);
+          }, 200)
+        });
+        $('#scrolltop').click(function(){
+          $('html, body').animate({scrollTop : 0},800);
+          return false;
+        });
+    })
       var scrollToFunc = function(element){
         $('html, body').animate({
           scrollTop: $("#"+element).offset().top
@@ -123,6 +130,40 @@
     )
   }))
 
- 
+  var form = document.getElementById("my-form");
+    
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var errorStatus = document.getElementById("my-form-error");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        status.classList.remove('d-none')
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            errorStatus.innerHTML = data["errors"].map(error => error["code"]==="TYPE_EMAIL"? 'Invalid Email': error["message"]).join(", ")
+            errorStatus.classList.remove('d-none')
+          } else {
+            errorStatus.innerHTML = "Oops! There was a problem submitting your form"
+          }
+          errorStatus.classList.remove('d-none')
+        })
+      }
+    }).catch(error => {
+      errorStatus.innerHTML = "Oops! There was a problem submitting your form"
+      errorStatus.classList.remove('d-none')
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
 
 })();
